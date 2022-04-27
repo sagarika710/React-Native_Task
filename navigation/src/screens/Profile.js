5bdf12837d17b5636e82de064699318d677c4ac8
deeptidewani014 <techiedeeptipadhy0722@gmail.com>
2022-04-27 12:16:15 +0530

    drwaer navigation

--- navigation/src/screens/Profile.js
+++ navigation/src/screens/Profile.js
@@ -9,95 +9,126 @@ export default function Profile() {
     const navigation = useNavigation();
 
     return (
-        <ScrollView style={styles.ProfileContainer}>
-            <View style={{}}>
-                <View style={{ marginVertical: 5, flexDirection: "row", alignItems: "center", marginHorizontal: 10 }}>
-                    <TouchableOpacity style={{ height: 40, width: 40, borderRadius: 50, alignItems: "center", justifyContent: "center", backgroundColor: "white", }} onPress={() => {
-                        navigation.goBack()
-                    }} >
-                        <Ionicons name="chevron-back-outline" color={"gray"} size={20} />
-                    </TouchableOpacity>
+        <View style={styles.ProfileContainer}>
+            <ScrollView >
+                <View style={{}}>
+                    <View style={{ marginVertical: 5, flexDirection: "row", alignItems: "center", marginHorizontal: 10 }}>
+                        <TouchableOpacity style={{ height: 40, width: 40, borderRadius: 50, alignItems: "center", justifyContent: "center", backgroundColor: "white", }} onPress={() => {
+                            navigation.goBack()
+                        }} >
+                            <Ionicons name="chevron-back-outline" color={"gray"} size={20} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ marginVertical: 10, flexDirection: "row", justifyContent: "space-around" }}>
+                        <View style={{ alignItems: "center" }}>
+                            <View style={{
+                                height: 110, width: 110,
+                                alignItems: "center", justifyContent: "center",
+                                borderRadius: 70
+                            }}>
+                                <Image style={{
+                                    height: 92.74, width: 100,
+                                    borderRadius: 70,
+                                    backgroundColor: "#01e0c5",
+
+                                }}
+                                    source={require("../assests/image2.png")}
+                                    resizeMode={"cover"}
+                                />
+                            </View>
+                            <View>
+                                <Text style={{ textAlign: "center", marginTop: 10, fontSize: 20, lineHeight: 25.1, textTransform: "capitalize" }}>Dr Kate Rose</Text>
+                            </View>
+                            <View>
+                                <Text style={{ textAlign: "center", marginTop: 5, fontSize: 14, color: "#01e0c5", textTransform: "capitalize", letterSpacing: 1 }}>pediatrician</Text>
+                            </View>
+                        </View>
+                    </View>
+                    {/* start here for  */}
+                    <View style={styles.CardContainer}>
+                        <View style={styles.innerCardContainer}>
+                            <View style={styles.detailsCardContainer}>
+                                <Ionicons name="people-outline" color="#7ACEFA" size={30} />
+                            </View>
+                            <Text>1000+</Text>
+                            <Text>Patients</Text>
+                        </View>
+                        {/*  */}
+                        <View style={styles.innerCardContainer}>
+                            <View style={styles.detailsCardContainer2}>
+                                <Ionicons name="ribbon-outline" color="#C7E80040" size={30} />
+                            </View>
+                            <Text>10Yrs</Text>
+                            <Text>Experience</Text>
+                        </View>
+                        {/*  */}
+                        <View style={styles.innerCardContainer}>
+                            <View style={styles.detailsCardContainer1}>
+                                <Ionicons name="star-outline" color="#F9BC0B63" size={30} />
+                            </View>
+                            <Text>4.5</Text>
+                            <Text>Ratings</Text>
+                        </View>
+                    </View>
                 </View>
-                <View style={{ marginVertical: 10, flexDirection: "row", justifyContent: "space-around" }}>
-                    <View style={{ alignItems: "center" }}>
-                        <View style={{
-                            height: 110, width: 110,
-                            alignItems: "center", justifyContent: "center",
-                            borderRadius: 70
-                        }}>
-                            <Image style={{
-                                height: 92.74, width: 100,
-                                borderRadius: 70,
-                                backgroundColor: "#01e0c5",
 
-                            }}
-                                source={require("../assests/image2.png")}
-                                resizeMode={"cover"}
-                            />
-                        </View>
-                        <View>
-                            <Text style={{ textAlign: "center", marginTop: 10, fontSize: 20, lineHeight: 25.1, textTransform: "capitalize" }}>Dr Kate Rose</Text>
-                        </View>
-                        <View>
-                            <Text style={{ textAlign: "center", marginTop: 5, fontSize: 14, color: "#01e0c5", textTransform: "capitalize", letterSpacing: 1 }}>pediatrician</Text>
-                        </View>
-                    </View>
+                {/*  */}
+                {/* AboutDoctor */}
+                <View style={{ marginTop: 15, paddingHorizontal: 10 }}>
+                    <Text style={{ fontSize: 18, fontFamily: "Mulish", letterSpacing: 0.3 }}>About Doctor</Text>
+                    <Text>
+                        Dr. Kate Rose is a top specialist at Delhi AIIMS Hospital. She has achieved several awards and recognition for is contribution and service in her own field. She is available for private consultation.
+                    </Text>
                 </View>
-                {/* start here for  */}
-                <View style={styles.CardContainer}>
-                    <View style={styles.innerCardContainer}>
-                        <View style={styles.detailsCardContainer}>
-                            <Ionicons name="people-outline" color="#7ACEFA" size={30} />
-                        </View>
-                        <Text>1000+</Text>
-                        <Text>Patients</Text>
-                    </View>
-                    {/*  */}
-                    <View style={styles.innerCardContainer}>
-                        <View style={styles.detailsCardContainer}>
-                            <Ionicons name="user-friends" color="#7ACEFA" size={30} />
-                        </View>
-                        <Text>1000+</Text>
-                        <Text>Patients</Text>
-                    </View>
-                    {/*  */}
-                    <View style={styles.innerCardContainer}>
-                        <View style={styles.detailsCardContainer}>
-                            <Ionicons name="user-friends" color="#7ACEFA" size={30} />
-                        </View>
-                        <Text>1000+</Text>
-                        <Text>Patients</Text>
-                    </View>
+                <View style={{ marginTop: 20, paddingHorizontal: 10 }}>
+                    <Text style={{ fontSize: 18, fontFamily: "Mulish" }}>Work Time</Text>
+                    <Text style={{ fontSize: 12, fontFamily: "Mulish", letterSpacing: 0.2, lineHeight: 20, fontStyle: "normal" }}>Mon - Sat (08:30 AM - 09:00 PM)</Text>
                 </View>
-            </View>
+                <View style={{ marginTop: 10, paddingHorizontal: 10 }}>
+                    <Text style={{ fontSize: 18, fontFamily: "Mulish" }}>Services</Text>
+                    <View style={{ marginBottom: 10, borderWidth: 1, padding: 3, borderColor: "#00E0C5", borderRadius: 5 }}>
+                        <TouchableOpacity style={{ flexDirection: "row" }}>
+                            <View style={{ backgroundColor: "#7ACEFA26", borderColor: "#7ACEFA", width: 70, height: 70, alignItems: "center", justifyContent: "center", borderRadius: 10, marginRight: 5 }}>
+                                <Ionicons name="call" color="#7ACEFA" size={30} />
+                            </View>
+                            <View>
+                                <Text style={{ fontSize: 16, fontFamily: "Mulish" }}>Call a doctor at home</Text>
+                                <Text style={{ fontSize: 10 }}>Get diagnosed in the comfort of your Home</Text>
+                            </View>
 
-            {/*  */}
-            {/* AboutDoctor */}
-            <View style={{ marginTop: 10, paddingHorizontal: 10 }}>
-                <Text style={{ fontSize: 18, fontFamily: "Mulish" }}>About Doctor</Text>
-                <Text>
-                    Dr. Kate Rose is a top specialist at Delhi AIIMS Hospital. She has achieved several awards and recognition for is contribution and service in her own field. She is available for private consultation.
-                </Text>
-            </View>
-            <View style={{ marginTop: 10, paddingHorizontal: 10 }}>
-                <Text style={{ fontSize: 18, fontFamily: "Mulish" }}>Work Time</Text>
-                <Text>Mon - Sat (08:30 AM - 09:00 PM)</Text>
-            </View>
-            <View style={{ marginTop: 10, paddingHorizontal: 10 }}>
-                <Text style={{ fontSize: 18, fontFamily: "Mulish" }}>Services</Text>
-                <TouchableOpacity style={{ flexDirection: "row" }}>
-                    <View style={{ backgroundColor: "#7ACEFA26" }}>
-                        <Ionicons name="call" color="#7ACEFA" size={30} />
+                        </TouchableOpacity>
                     </View>
-                    <View>
-                        <Text>Call a doctor at home</Text>
-                        <Text>Get diagnosed in the comfort. of your home.</Text>
+                    <View style={{ marginBottom: 10, padding: 3, borderColor: "#7ACEFA", borderRadius: 5 }}>
+                        <TouchableOpacity style={{ flexDirection: "row" }}>
+                            <View style={{ backgroundColor: "#C7E8004F", borderColor: "#C7E8004F", width: 70, height: 70, alignItems: "center", justifyContent: "center", borderRadius: 10, marginRight: 5 }}>
+                                <FontAwesome5Icons name="calendar" color="#C7E8004F" size={30} />
+                            </View>
+                            <View>
+                                <Text style={{ fontSize: 16, fontFamily: "Mulish" }}>Call a doctor at home</Text>
+                                <Text style={{ fontSize: 10 }}>Get diagnosed in the comfort of your Home</Text>
+                            </View>
+
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ padding: 3, borderColor: "#FBBC05", borderRadius: 5 }}>
+                        <TouchableOpacity style={{ flexDirection: "row" }}>
+                            <View style={{ backgroundColor: "#FBBC05", borderColor: "#7ACEFA", width: 70, height: 70, alignItems: "center", justifyContent: "center", borderRadius: 10, marginRight: 5 }}>
+                                <Ionicons name="videocam-outline" size={30} />
+                            </View>
+                            <View>
+                                <Text style={{ fontSize: 16, fontFamily: "Mulish" }}>Call a doctor at home</Text>
+                                <Text style={{ fontSize: 10 }}>Get diagnosed in the comfort of your Home</Text>
+                            </View>
+
+                        </TouchableOpacity>
                     </View>
 
-                </TouchableOpacity>
-            </View>
 
-        </ScrollView>
+                </View>
+
+            </ScrollView >
+
+        </View >
     )
 }
 const styles = StyleSheet.create({
@@ -128,6 +159,24 @@ const styles = StyleSheet.create({
     },
     detailsCardContainer: {
         backgroundColor: "#7ACEFA26",
+        height: 63, width: 49,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        paddingVertical: 5
+    },
+    detailsCardContainer1: {
+        backgroundColor: "#F9BC0B63",
+        height: 63, width: 49,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20,
+        paddingVertical: 5
+    },
+    detailsCardContainer2: {
+        backgroundColor: "#C7E80040",
         height: 63, width: 49,
         justifyContent: "flex-end",
         alignItems: "center",
